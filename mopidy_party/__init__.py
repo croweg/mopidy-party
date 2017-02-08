5f7e98be9eaebb5b85e9dbdbb7d85ee92df6d492
croweg <2scratch5@gmail.com>
2017-02-08 10:21:45 +0100

    Cleaned

--- mopidy_party/__init__.py
+++ mopidy_party/__init__.py
@@ -10,7 +10,7 @@ class Extension(ext.Extension):
     dist_name = 'Mopidy-Party'
     ext_name = 'party'
     version = __version__
-
+    
     def get_default_config(self):
         conf_file = os.path.join(os.path.dirname(__file__), 'ext.conf')
         return config.read(conf_file)
