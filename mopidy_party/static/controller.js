5be873f201d302a6b4c046c6dae6d54d6eead87b
croweg <2scratch5@gmail.com>
2017-02-19 13:45:49 +0100

    Add files via upload

--- mopidy_party/static/controller.js
+++ mopidy_party/static/controller.js
@@ -60,8 +60,8 @@ angular.module('partyApp', [])
     $scope.$apply();
   });
   mopidy.on('event:tracklistChanged', function(){
-    mopidy.tracklist.getLength().done(function(length){
-      $scope.currentState.length = length;
+    mopidy.tracklist.getTlTracks().done(function(track){
+      $scope.tltracks.name = track;
       $scope.$apply();
     });
   });
