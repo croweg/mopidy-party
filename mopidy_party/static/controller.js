5a10159da1fa01913c54c8e3c5994967327d8bf3
croweg <2scratch5@gmail.com>
2017-03-15 11:23:36 +0100

    Update controller.js

--- mopidy_party/static/controller.js
+++ mopidy_party/static/controller.js
@@ -19,7 +19,7 @@ angular.module('partyApp', [])
       name   : 'Nothing playing, add some songs to get the party going!'
     }
   };
-console.log("extName: " + $scope.tltracks.name)
+console.log("extName: " + $scope.tltracks)
   // Initialize
 
   var mopidy = new Mopidy({
