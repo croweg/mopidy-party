53de98302fa923ffefe58f5a638a4308607d044a
croweg <2scratch5@gmail.com>
2017-03-15 10:54:32 +0100

    Update controller.js

--- mopidy_party/static/controller.js
+++ mopidy_party/static/controller.js
@@ -8,6 +8,7 @@ angular.module('partyApp', [])
 
   $scope.message = [];
   $scope.tracks  = [];
+  $scope.tltracks = ["Aucune musique dans la playlist"];
   $scope.loading = true;
   $scope.ready   = false;
   $scope.currentState = {
