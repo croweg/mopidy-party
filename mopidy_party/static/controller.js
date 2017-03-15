d25048fa87ccd61f58c98951fc9629c5b4cce548
croweg <2scratch5@gmail.com>
2017-03-15 09:40:34 +0100

    Add files via upload

--- mopidy_party/static/controller.js
+++ mopidy_party/static/controller.js
@@ -66,7 +66,7 @@ console.log("extName: " + $scope.tltracks.name)
 			console.log("keys: " + keys);
 			for (var i = 0, len = keys.length; i < len; i++) {
 				console.log("Name: " + tltrack[i]["name"]);
-				$scope.tltracks.name = tltrack[i]["name"];
+				$scope.tltracks.name.push(tltrack[i]["name"]);
 				console.log("ExtName: " + $scope.tltracks.name)}
 		});
 	});
