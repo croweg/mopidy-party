2d99d1d64a3d45163e11eac91be045824ab9a1e4
croweg <2scratch5@gmail.com>
2017-03-15 19:15:56 +0100

    Update controller.js

--- mopidy_party/static/controller.js
+++ mopidy_party/static/controller.js
@@ -153,7 +153,7 @@ angular.module('partyApp', [])
     })
     .then(function(){
       // Notify user
-      $scope.message = ['success', track.name ' à été ajouté à la playlist'];
+      $scope.message = ['success', track.name + ' à été ajouté à la playlist'];
       $scope.$apply();
       return mopidy.tracklist.setConsume([true]);
     })
